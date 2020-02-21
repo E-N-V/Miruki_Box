d129aa6da99abd05556c3b83101fd19f7ff53664
Egor <39993671+s1ither@users.noreply.github.com>
2020-02-21 18:00:57 +0500

    Alpha

---
+++ app.js
@@ -0,0 +1,55 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const dev = true;
+
+//Routes
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Middleware
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  if (!dev) {
+    let rnd = Math.floor(Math.random()*(5-1)+1);
+    switch (rnd) {
+      case 1: res.status(404).render('404/demo1');
+        break;
+      case 2: res.status(404).render('404/demo2');
+        break;
+      case 3: res.status(404).render('404/demo3');
+        break;
+      case 4: res.status(404).render('404/demo4');
+        break;
+    }
+  }else res.render('404/error');
+});
+
+module.exports = app;
