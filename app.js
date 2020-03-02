acece6a921decf039bb2dbc100d998feedac0d03
S1ither <ophcik@mail.ru>
2020-03-02 17:13:43 +0500

    Перекинул в серверную часть, комит рабочий.

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const dev = true;
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const dev = false;
 
 //Routes
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const testRouter = require('./routes/test');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Middleware
 app.use('/', indexRouter);
+app.use('/index.html', indexRouter);
+app.use('/index', indexRouter);
 app.use('/users', usersRouter);
+app.use('/test', testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
