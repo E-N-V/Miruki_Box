466dab224ed626c7cc65be292fa69e0c64be51db
QuirkyBirdy <sm.nk@inbox.ru>
2020-03-14 17:56:49 +0300

    nav fix

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -5,5 +5,11 @@ window.onload = () =>{
 function navOpen(){
     let menuButton = document.getElementsByClassName('nav-main');
     let menuContent = document.getElementsByClassName('nav-content');
-    menuContent[0].style.display = 'flex'
+    if(menuContent[0].value == '1'){
+        menuContent[0].style.display = 'none'
+        menuContent[0].value = '0'
+    }else{
+        menuContent[0].style.display = 'flex'
+        menuContent[0].value = '1'
+    }
 }
