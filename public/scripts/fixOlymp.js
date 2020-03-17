0597cb106ffe1e82687d95cb318ea76490dd8fb1
S1ither <ophcik@mail.ru>
2020-03-17 18:39:31 +0500

    Fix
    Fix somes bug

--- public/scripts/fixOlymp.js
+++ public/scripts/fixOlymp.js
@@ -1,38 +1,96 @@
-window.onload = () =>{
-    const d = document;
-    const button = [];
-    for (const item of redactorArea.getElementsByTagName('button')) button.push(item);
-    for (let i = 0; i < button.length; i++) button[i].addEventListener('click', test(button[i]))
+function reDraw(block, mode){
+    switch (mode.id) {
+        case "radio":
+            block.innerHTML = `
+                <button class="goBack controls" onclick="goBack(document.getElementById('redactorArea'))"><</button>
+                <textarea name="" id="testArea" cols="30" rows="10"></textarea>
+                <div class="answersContainer" id="AnswerContainer">
+                    <div class="answer">
+                        <span>a)</span>
+                        <input type="text" name="" id="text" class="input">
+                        <div class="radio"></div>
+                        <button class="controls delete" style="display: none">-</button>
+                    </div>
+                </div>
+                <button class="controls append" name="radio" onclick="AddAnswer(document.getElementById('AnswerContainer'), this)">+</button>
+                `;
+                break;
+        case "checkbox":
+            block.innerHTML = `
+                <button class="goBack controls" onclick="goBack(document.getElementById('redactorArea'))"><</button>
+                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                    <div class="answersContainer">
+                        <div class="answer">
+                            <span>a)</span>
+                            <input type="text" name="" id="" class="input">
+                            <div class="checkbox"></div>
+                            <button class="controls delete" style="display:none">-</button>
+                        </div>
+                    </div>
+                    <button class="controls append" name="checkbox" onclick="AddAnswer(document.getElementById('AnswerContainer'), this)">+</button>
+                `;
+            break;
+        case "textbox":
+            block.innerHTML = `
+                <button class="goBack controls" onclick="goBack(document.getElementById('redactorArea'))"><</button>
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <input type="text" name="" id="">
+                `;
+            break;
+    }
 }
 
-function reDraw(block){
-    block.innerHTML = `
-    <button class="goBack controls"><</button>
-    <textarea name="" id="testArea" cols="30" rows="10"></textarea>
-    <div class="answersContainer" id="AnswerContainer">
-        <div class="answer">
+function AddAnswer(block, mode){
+    const div = document.createElement('div');
+    switch (mode.name) {
+        case "textbox":
+            div.className = 'answer';
+            div.innerHTML = `
             <span>a)</span>
-            <input type="text" name="" id="texta" class="input">
+            <input type="text" name="" id="text" class="input">
             <div class="radio"></div>
-            <button class="controls delete" style="display:none">-</button>
-        </div>
-    </div>
-    <button class="controls append" onclick="AddAnswer(document.getElementById('AnswerContainer'))">+</button>
-`;
+            <button class="controls delete">-</button>
+            `;
+            block.append(div);
+            break;
+        case "checkbox":
+            div.className = 'answer';
+            div.innerHTML = `
+            <span>a)</span>
+            <input type="text" name="" id="text" class="input">
+            <div class="radio"></div>
+            <button class="controls delete">-</button>
+            `;
+            block.append(div);
+            break;
+        case "radio":
+            div.className = 'answer';
+            div.innerHTML = `
+            <span>a)</span>
+            <input type="text" name="" id="text" class="input">
+            <div class="radio"></div>
+            <button class="controls delete" onclick="delAnswer(document.getElementsByClassName('answer')[document.getElementsByClassName('answer').length - 1])">-</button>
+            `;
+            block.append(div);
+            break;
+    }
 }
 
-function AddAnswer(block){
-    let div = document.createElement('div');
-    div.className = 'answer'
-    div.innerHTML = `
-            <span>a)</span>
-            <input type="text" name="" id="texta" class="input">
-            <div class="radio"></div>
-            <button class="controls delete" style="display:none">-</button>
-    `;
-    block.append(div);
+function delAnswer(block) {
+    block.shift(block);
 }
 
 function AddQuestion(){
 
+}
+
+function goBack(block) {
+    block.innerHTML = `
+    <div class="question" id="question">
+                    <h2>Выбирете тип вопроса:</h2>
+                    <button class="questMode" id="radio" onclick="reDraw(document.getElementById('question'),  this)"><img src="/images/radio.svg" alt=""><span>Вопрос имеющий один ответ</span></button>
+                    <button class="questMode" id="checkbox" onclick="reDraw(document.getElementById('question'), this)"><img src="/images/radio.svg" alt=""><span>Вопрос имеющий несколько ответов</span></button>
+                    <button class="questMode" id="textbox" onclick="reDraw(document.getElementById('question'), this)"><img src="/images/radio.svg" alt=""><span>Вопрос на который нужно дать письменный ответ (точное число или слово)</span></button>
+                </div>
+    `;
 }
