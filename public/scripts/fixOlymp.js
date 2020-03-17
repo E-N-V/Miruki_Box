faa5f940f66adb661592eb79133f760f624dd86e
S1ither <39993671+S1ither@users.noreply.github.com>
2020-03-17 17:18:21 +0500

    Fix
    Переделывание конструктора вопросов

--- public/scripts/fixOlymp.js
+++ public/scripts/fixOlymp.js
@@ -1,6 +1,38 @@
-window.onload = () => {
+window.onload = () =>{
+    const d = document;
+    const button = [];
+    for (const item of redactorArea.getElementsByTagName('button')) button.push(item);
+    for (let i = 0; i < button.length; i++) button[i].addEventListener('click', test(button[i]))
+}
 
-};
+function reDraw(block){
+    block.innerHTML = `
+    <button class="goBack controls"><</button>
+    <textarea name="" id="testArea" cols="30" rows="10"></textarea>
+    <div class="answersContainer" id="AnswerContainer">
+        <div class="answer">
+            <span>a)</span>
+            <input type="text" name="" id="texta" class="input">
+            <div class="radio"></div>
+            <button class="controls delete" style="display:none">-</button>
+        </div>
+    </div>
+    <button class="controls append" onclick="AddAnswer(document.getElementById('AnswerContainer'))">+</button>
+`;
+}
 
-function chck(){
+function AddAnswer(block){
+    let div = document.createElement('div');
+    div.className = 'answer'
+    div.innerHTML = `
+            <span>a)</span>
+            <input type="text" name="" id="texta" class="input">
+            <div class="radio"></div>
+            <button class="controls delete" style="display:none">-</button>
+    `;
+    block.append(div);
+}
+
+function AddQuestion(){
+
 }
--- routes/certificateRouter.js
+++ routes/certificateRouter.js
@@ -4,6 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     //any function
+    res.render('certificate');
 });
+router.get('/find/', (req,res,next) => {
+    console.log(req.query.usr);
+    res.status(200).send('<div> Hello </div>');
+})
 
 module.exports = router;
