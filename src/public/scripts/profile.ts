d26b70affbb2342fbc43fb67cd24402cc9bc1162
QuirkyBirdy <sm.nk@inbox.ru>
2020-05-13 11:06:24 +0500

    change role button

--- src/public/scripts/profile.ts
+++ src/public/scripts/profile.ts
@@ -1,3 +1,38 @@
+
+function chpok(){
+    const something:any = document.getElementsByClassName('profileChange')[0];
+    console.log("chpok")
+    something.style.display = 'flex'
+    something.innerHTML+=`
+        <div class="changeMenu">
+            <div class="close" onclick="closeCh()">X</div>
+            <p> Изменение профиля </p>
+            <form action="">
+                <input type="hidden", name="userId" value="">              
+                <span> ученик
+                    <input type="radio", name="role" value="pupil">
+                </span> 
+                <span> ученик + 
+                    <input type="radio", name="role" value="turbopupil">
+                </span>
+                <span> учитель 
+                    <input type="radio", name="role" value="teacher">
+                </span>
+                <button> Подтвердить </button>
+            </form>
+        </div>
+    `
+}
+
+
+function closeCh(){
+    var something:any = document.getElementsByClassName('changeMenu')[0];
+    something.remove();
+    something = document.getElementsByClassName('profileChange')[0];
+    console.log("chpok")
+    something.style.display = 'none'
+}
+
 function createReward( medalText:string, olympName:any, olympTime:any, olympPlace:any ){
     const something:any = document.getElementById("olymps");
     const cat:any = document.getElementById('cat');
